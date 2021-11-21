358f5aa9f228abf7b6ebfaebcdf94e99a18c9514
Takhir <60847855+TakhirFox@users.noreply.github.com>
2021-11-21 22:12:13 +0700

    Add background image for cell

--- Chiff/ImageCell.swift
+++ Chiff/ImageCell.swift
@@ -10,18 +10,30 @@ import UIKit
 class ImageCell: UICollectionViewCell {
     
     let imageView = UIImageView()
+    let imageViewBackground = UIImageView()
     let loadButton = UIButton()
-    
+    let blurEffect = UIBlurEffect(style: .light)
+    var blurredEffectView = UIVisualEffectView()
+   
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         loadButton.isHidden = true
-                
+        imageView.contentMode = .scaleAspectFit
+
+        imageViewBackground.frame = bounds
+        imageViewBackground.contentMode = .scaleToFill
+        blurredEffectView = UIVisualEffectView(effect: blurEffect)
+        blurredEffectView.frame = imageViewBackground.bounds
+        
+        addSubview(imageViewBackground)
+        addSubview(blurredEffectView)
         addSubview(imageView)
         addSubview(loadButton)
         
         loadButton.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
         imageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        imageViewBackground.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
     }
     
     required init?(coder: NSCoder) {
