1f8d8d154e60c2cf975c659aa654dca36c27366c
Takhir <60847855+TakhirFox@users.noreply.github.com>
2021-11-21 22:15:38 +0700

    Delete UIImagePickerController

--- Chiff/CreatePostImageButtonCell.swift
+++ Chiff/CreatePostImageButtonCell.swift
@@ -12,9 +12,8 @@ class CreatePostImageButtonCell: UICollectionViewCell {
     // Код, над которым я плачу по ночам, вроде бы и работает, но хочу по архитектуре а спросить не у кого, хочу правильно, а не вот это вот все :(((((((
     let titleLabel = UILabel()
     var images = [UIImage]() // Храним массив изображений
-    var imagePicker: UIImagePickerController!
-    weak var viewController: UIViewController? // Как то нужно было презентить алерт)
-    
+    let button = UIButton()
+
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -24,11 +23,8 @@ class CreatePostImageButtonCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        // Делегируем работу UIImagePicker на себя
-        imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.allowsEditing = true
+
+        button.backgroundColor = .red
         
         // Тут думаю и так понятно
         collectionView.delegate = self
@@ -41,10 +37,11 @@ class CreatePostImageButtonCell: UICollectionViewCell {
         
         addSubview(titleLabel)
         addSubview(collectionView)
+        addSubview(button)
         
         titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: 20))
-        collectionView.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, size: .init(width: 0, height: 0))
-
+        collectionView.anchor(top: titleLabel.bottomAnchor, leading: button.trailingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, size: .init(width: 0, height: 0))
+        button.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: collectionView.leadingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 100, height: 0))
     }
     
     required init?(coder: NSCoder) {
@@ -57,64 +54,32 @@ class CreatePostImageButtonCell: UICollectionViewCell {
 // MARK: - Actions
 extension CreatePostImageButtonCell {
     
-    // Вызываем Алерт
-    @objc func loadImage() {
-        let alert = UIAlertController(title: "Загрузить изображение", message: nil, preferredStyle: .actionSheet)
-        let cameraAction = UIAlertAction(title: "Камера", style: .default) { _ in
-            self.imagePicker.sourceType = .camera
-            self.viewController?.present(self.imagePicker, animated: true, completion: nil)
+    func reloadCell() {
+        DispatchQueue.main.async() {
+            print("XXXASSXSXSX \(self.images)")
+            self.collectionView.reloadData()
         }
-        let libraryAction = UIAlertAction(title: "Галерея", style: .default) { _ in
-            self.imagePicker.sourceType = .photoLibrary
-            self.viewController?.present(self.imagePicker, animated: true, completion: nil)
-        }
-        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
-        alert.addAction(cameraAction)
-        alert.addAction(libraryAction)
-        alert.addAction(cancelAction)
-        self.viewController?.present(alert, animated: true, completion: nil)
     }
 }
 
 extension CreatePostImageButtonCell: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
-    }
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        } else {
-            return images.count
-        }
+        return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if indexPath.section == 0 {
-            // Тут одна кнопка для загрузки картинки
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! ImageCell
-            cell.loadButton.isHidden = false
-            cell.imageView.isHidden = true
-            
-            cell.loadButton.backgroundColor = .systemBlue
-            cell.loadButton.addTarget(self, action: #selector(loadImage), for: .touchUpInside)
-            
-            
-            cell.loadButton.layer.cornerRadius = 8
-            cell.loadButton.clipsToBounds = true
-            return cell
-        } else {
-            // Тут загруженные картинки
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! ImageCell
-            cell.loadButton.isHidden = true
-            cell.imageView.isHidden = false
-            
-            cell.imageView.image = images[indexPath.row]
-            cell.imageView.layer.cornerRadius = 8
-            cell.imageView.clipsToBounds = true
-            return cell
-        }
+        
+        // Тут загруженные картинки
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! ImageCell
+        cell.loadButton.isHidden = true
+        cell.imageView.isHidden = false
+        
+        cell.imageView.image = images[indexPath.row]
+        cell.imageView.layer.cornerRadius = 8
+        cell.imageView.clipsToBounds = true
+        return cell
+
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -122,23 +87,4 @@ extension CreatePostImageButtonCell: UICollectionViewDataSource, UICollectionVie
     }
     
     
-}
-
-// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
-extension CreatePostImageButtonCell: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let image = info[.editedImage] as? UIImage else {
-            return
-        }
-        // Добавляем в начало массива картинку с помощью insert at: начало массива
-        self.images.insert(image, at: 0)
-
-        collectionView.reloadData()
-        
-        imagePicker.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        imagePicker.dismiss(animated: true, completion: nil)
-    }
 }
