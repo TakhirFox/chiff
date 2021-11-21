46eb9e494c6649df05259308a4ae611669f49739
Takhir <60847855+TakhirFox@users.noreply.github.com>
2021-11-21 22:11:32 +0700

    Create new model

---
+++ Chiff/Model/Entity/CreatePostModel.swift
@@ -0,0 +1,16 @@
+//
+//  CreatePostModel.swift
+//  Chiff
+//
+//  Created by Zakirov Tahir on 21.11.2021.
+//
+
+import UIKit
+
+struct CreatePostModel {
+    var title: String?
+    var content: String?
+    var status: String?
+    var images: [UIImage]?
+    var cost: String?
+}
