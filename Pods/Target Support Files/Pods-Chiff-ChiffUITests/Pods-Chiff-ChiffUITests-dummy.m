5987cffe5d427439d113b8094aef93a46e414fd5
Тахир <iamtheorangefox@gmail.com>
2021-12-09 12:47:49 +0700

    Remove Pods

--- Pods/Target Support Files/Pods-Chiff-ChiffUITests/Pods-Chiff-ChiffUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Chiff_ChiffUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Chiff_ChiffUITests
-@end
--- Pods/Target Support Files/Pods-Chiff/Pods-Chiff-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Chiff : NSObject
-@end
-@implementation PodsDummy_Pods_Chiff
-@end
