5d7320864750ba9eddc675db8630c326b5772a4c
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-02-27 14:06:50 +0700

    Добавил конфигурацию внутри ячейки

--- Chiff/FeedCell.swift
+++ Chiff/FeedCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class FeedCell: UICollectionViewCell {
     
@@ -28,6 +29,9 @@ class FeedCell: UICollectionViewCell {
         layer.cornerRadius = 8
         layer.masksToBounds = true
         
+        configureSubviews()
+        configureConstraints()
+        
         titleLabel.numberOfLines = 0
         titleLabel.font = UIFont(name: "ArialMT", size: 15)
 
@@ -38,14 +42,30 @@ class FeedCell: UICollectionViewCell {
         authorLabel.font = UIFont(name: "Arial-BoldMT", size: 12)
         authorLabel.textColor = .white
         
+    }
+    
+    func setupCell(_ post: News) {
+        titleLabel.text = post.title?.rendered
+        authorLabel.text = nil
+        imageView.image = UIImage(named: "no_image")
+        authorLabel.text = post.authorName
+        
+        if let imageUrl = post.imagesPost, !imageUrl.isEmpty {
+            let urlString = URL(string: imageUrl[0])
+            imageView.kf.setImage(with: urlString)
+        }
+    }
+    
+    func configureSubviews() {
         addSubview(imageView)
         addSubview(titleLabel)
         addSubview(authorLabel)
-        
+    }
+    
+    func configureConstraints() {
         imageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
         titleLabel.anchor(top: imageView.bottomAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 4, left: 4, bottom: 4, right: 4), size: .init(width: 0, height: 40))
         authorLabel.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 4, left: 4, bottom: 4, right: 4), size: .init(width: 0, height: 40))
-        
     }
     
     required init?(coder: NSCoder) {
