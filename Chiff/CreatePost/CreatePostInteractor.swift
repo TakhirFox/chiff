aa86b1d2d718c3302682b007e4ea82501da0fc61
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-03-09 00:02:59 +0700

    Добавил 2 новых запроса

--- Chiff/CreatePost/CreatePostInteractor.swift
+++ Chiff/CreatePost/CreatePostInteractor.swift
@@ -8,14 +8,42 @@
 import Foundation
 
 protocol CreatePostInteractorProtocol {
+    func getCategories()
+    func createPost(post: CreatePostModel)
     
 }
 
 class CreatePostInteractor: BaseInteractor, CreatePostInteractorProtocol {
     weak var presenter: CreatePostPresenterProtocol?
-    var networkService: NetworkService?
+    var networkService: NetworkService!
+    
 }
 
 extension CreatePostInteractor {
+    func getCategories() {
+        networkService.getCategories { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let categories):
+                self.presenter?.showCategorySuccess(cat: categories)
+            case .failure(let error):
+                self.presenter?.getCategoryError(error: "\(error)")
+            }
+        }
+    }
+    
+    func createPost(post: CreatePostModel) {
+        networkService!.createNewPost(post: post, complitionHandler: { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let createdPost):
+                self.presenter?.showCreatePostSuccess(post: createdPost)
+            case .failure(let error):
+                self.presenter?.showCreatePostError("\(error)")
+            }
+        })
+    }
     
 }
