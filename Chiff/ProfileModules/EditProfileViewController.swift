9a4792f1e1099cbe9545587f72094eae5dee4c3f
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-03-10 00:13:26 +0700

    Добавил табличное вью

--- Chiff/ProfileModules/EditProfileViewController.swift
+++ Chiff/ProfileModules/EditProfileViewController.swift
@@ -13,22 +13,55 @@ protocol EditProfileViewControllerProtocol: AnyObject {
 
 class EditProfileViewController: BaseViewController, EditProfileViewControllerProtocol {
     
+    var tableView: UITableView!
+    
+    var user: User?
     var presenter: EditProfilePresenterProtocol?
     
     override func viewDidLoad() {
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
+        activityIndicator.startAnimating()
         
+        setupTableView()
         setupSubviews()
         setupConstraints()
     }
     
+    func setupTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+//        tableView.isHidden = true
+//        tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+    }
+    
     func setupSubviews() {
-       
+        view.addSubview(tableView)
+        view.addSubview(activityIndicator)
     }
     
     func setupConstraints() {
-    
+        tableView.snp.makeConstraints { make in
+            make.top.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        activityIndicator.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
     }
+}
+
+extension EditProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+   
 }
 
 extension EditProfileViewController {
