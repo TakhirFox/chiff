3c383bf001acbb4a253d8a5bac30bd20fa56700d
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-03-11 00:04:07 +0700

    Оформил ячейку с textfield

--- Chiff/ProfileModules/Cells/EditTextFieldCell.swift
+++ Chiff/ProfileModules/Cells/EditTextFieldCell.swift
@@ -5,7 +5,55 @@
 //  Created by Zakirov Tahir on 10.03.2022.
 //
 
-import Foundation
+import UIKit
 import SnapKit
 
+class EditTextFieldCell: UITableViewCell {
+    let titleLabel: UILabel = {
+        let view = UILabel()
+        view.text = "Название"
+        return view
+    }()
+    
+    let textField: UITextField = {
+        let view = UITextField()
+        view.backgroundColor = .systemGray5
+        view.borderStyle = .roundedRect
+        view.text = "asd"
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        selectionStyle = .none
+        
+        configureSubviews()
+        configureConstraints()
+    }
+    
+    func configureSubviews() {
+        addSubview(titleLabel)
+        addSubview(textField)
+    }
+    
+    func configureConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(6)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(25)
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(6)
+            make.height.equalTo(50)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
