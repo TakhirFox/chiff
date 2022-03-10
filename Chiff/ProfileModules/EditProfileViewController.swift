415405bbf4cc9a686fe50651c5352634b8f604b4
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-03-11 00:04:26 +0700

    Зарегал ячейки, добавил enum

--- Chiff/ProfileModules/EditProfileViewController.swift
+++ Chiff/ProfileModules/EditProfileViewController.swift
@@ -13,6 +13,16 @@ protocol EditProfileViewControllerProtocol: AnyObject {
 
 class EditProfileViewController: BaseViewController, EditProfileViewControllerProtocol {
     
+    private enum Items: Int {
+        case imageItem = 0
+        case firstnameItem = 1
+        case lastnameItem = 2
+        case emailItem = 3
+        case bioItem = 4
+        case passwordItem = 5
+        case saveButtonItem = 6
+    }
+    
     var tableView: UITableView!
     
     var user: User?
@@ -31,8 +41,13 @@ class EditProfileViewController: BaseViewController, EditProfileViewControllerPr
         tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .none
 //        tableView.isHidden = true
-//        tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+        tableView.register(EditAvatarCell.self, forCellReuseIdentifier: "cell1")
+        tableView.register(EditTextFieldCell.self, forCellReuseIdentifier: "cell2")
+        tableView.register(EditButtonCell.self, forCellReuseIdentifier: "cell3")
+        setProfileInfo()
     }
     
     func setupSubviews() {
@@ -54,16 +69,69 @@ class EditProfileViewController: BaseViewController, EditProfileViewControllerPr
 
 extension EditProfileViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 7
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let items = Items(rawValue: indexPath.item)
+        switch items {
+        case .imageItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! EditAvatarCell
+            
+            return cell
+            
+        case .firstnameItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Имя"
+            cell.textField.delegate = self
+            return cell
+            
+        case .lastnameItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Фамилия"
+            cell.textField.delegate = self
+            return cell
+            
+        case .emailItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Почта"
+            cell.textField.delegate = self
+            return cell
+            
+        case .bioItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "О себе"
+            cell.textField.delegate = self
+            return cell
+            
+        case .passwordItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Пароль"
+            cell.textField.delegate = self
+            return cell
+            
+        case .saveButtonItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath) as! EditButtonCell
+            cell.actionButton.setTitle("Сохранить", for: .normal)
+            return cell
+            
+        case .none:
+            return UITableViewCell()
+        }
     }
     
-   
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("LOG: sda \(indexPath.row)")
+    }
+
+}
+
+extension EditProfileViewController: UITextFieldDelegate {
+    
 }
 
 extension EditProfileViewController {
-   
+    func setProfileInfo() {
+        self.activityIndicator.stopAnimating()
+    }
 }
