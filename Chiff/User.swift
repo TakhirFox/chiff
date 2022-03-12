ee36741a89073d254ea5132c016ad9401470f91a
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-03-12 21:49:42 +0700

    Изменение модели

--- Chiff/User.swift
+++ Chiff/User.swift
@@ -8,36 +8,47 @@
 import Foundation
 
 struct User: Decodable {
-    let id: Int?
-    let name: String?
-    let url: String?
-    let welcomeDescription: String?
-    let link: String?
-    let slug: String?
-    let links: Links?
-    let avatarimage: Avatarimage?
-    let numberphone, lastname, firstname: String?
+    var id: Int?
+    var name: String?
+    var url: String?
+    var welcomeDescription: String?
+    var link: String?
+    var slug: String?
+    var links: Links?
+    var userEmail: String?
+    var avatarimage: Avatarimage?
+    var numberphone, lastname, firstname: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, url
+        case welcomeDescription = "description"
+        case link, slug
+        case userEmail = "user_email"
+        case avatarimage, numberphone, lastname, firstname
+        case links = "_links"
+    }
+    
 }
 
 // MARK: - Avatarimage
 struct Avatarimage: Decodable {
-    let id, postAuthor, postDate, postDateGmt: String?
-    let postContent, postTitle, postExcerpt, postStatus: String?
-    let commentStatus, pingStatus, postPassword, postName: String?
-    let toPing, pinged, postModified, postModifiedGmt: String?
-    let postContentFiltered, postParent: String?
-    let guid: String?
-    let menuOrder, postType, postMIMEType, commentCount: String?
-    let podItemID: String?
+    var id, postAuthor, postDate, postDateGmt: String?
+    var postContent, postTitle, postExcerpt, postStatus: String?
+    var commentStatus, pingStatus, postPassword, postName: String?
+    var toPing, pinged, postModified, postModifiedGmt: String?
+    var postContentFiltered, postParent: String?
+    var guid: String?
+    var menuOrder, postType, postMIMEType, commentCount: String?
+    var podItemID: String?
 }
 
 // MARK: - Links
 struct Links: Decodable {
-    let linksSelf: [Collection]?
-    let collection: [Collection]?
+    var linksSelf: [Collection]?
+    var collection: [Collection]?
 }
 
 // MARK: - Collection
 struct Collection: Decodable {
-    let href: String?
+    var href: String?
 }
