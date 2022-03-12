7f43318c4ea6984e457837917aa97f47d0d322e3
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-03-12 21:51:18 +0700

    Добавил реализацию методов для презентера

--- Chiff/ProfileModules/EditProfileViewController.swift
+++ Chiff/ProfileModules/EditProfileViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 protocol EditProfileViewControllerProtocol: AnyObject {
     var presenter: EditProfilePresenterProtocol? { get set }
+    func setProfileInfo(user: User)
+    
+    func showEditProfileSuccess(user: User)
+    func showErrorProfileInfo(_ error: String)
+    func showEditProfileError(_ error: String)
 }
 
 class EditProfileViewController: BaseViewController, EditProfileViewControllerProtocol {
@@ -18,15 +23,22 @@ class EditProfileViewController: BaseViewController, EditProfileViewControllerPr
         case firstnameItem = 1
         case lastnameItem = 2
         case emailItem = 3
-        case bioItem = 4
-        case passwordItem = 5
-        case saveButtonItem = 6
+        case numberPhoneItem = 4
+        case bioItem = 5
+        case passwordItem = 6
+        case saveButtonItem = 7
     }
     
     var tableView: UITableView!
     
     var user: User?
+    var editUser = User()
     var presenter: EditProfilePresenterProtocol?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter?.getUsernameInfo()
+    }
     
     override func viewDidLoad() {
         view.backgroundColor = .systemBackground
@@ -43,11 +55,11 @@ class EditProfileViewController: BaseViewController, EditProfileViewControllerPr
         tableView.dataSource = self
         tableView.rowHeight = UITableView.automaticDimension
         tableView.separatorStyle = .none
-//        tableView.isHidden = true
+        tableView.keyboardDismissMode = .onDrag
+        tableView.isHidden = true
         tableView.register(EditAvatarCell.self, forCellReuseIdentifier: "cell1")
         tableView.register(EditTextFieldCell.self, forCellReuseIdentifier: "cell2")
         tableView.register(EditButtonCell.self, forCellReuseIdentifier: "cell3")
-        setProfileInfo()
     }
     
     func setupSubviews() {
@@ -69,7 +81,7 @@ class EditProfileViewController: BaseViewController, EditProfileViewControllerPr
 
 extension EditProfileViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
+        return 8
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -77,53 +89,69 @@ extension EditProfileViewController: UITableViewDataSource, UITableViewDelegate 
         switch items {
         case .imageItem:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! EditAvatarCell
-            
+            let urlString = URL(string: user?.avatarimage?.guid ?? "")
+            cell.avatarImageView.kf.setImage(with: urlString)
             return cell
             
         case .firstnameItem:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
             cell.titleLabel.text = "Имя"
+            cell.textField.text = user?.firstname
+            cell.textField.tag = 0
             cell.textField.delegate = self
             return cell
             
         case .lastnameItem:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
             cell.titleLabel.text = "Фамилия"
+            cell.textField.text = user?.lastname
+            cell.textField.tag = 1
             cell.textField.delegate = self
             return cell
             
         case .emailItem:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
             cell.titleLabel.text = "Почта"
+            cell.textField.text = user?.userEmail
+            cell.textField.tag = 2
+            cell.textField.keyboardType = .emailAddress
+            cell.textField.delegate = self
+            return cell
+            
+        case .numberPhoneItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Номер телефона"
+            cell.textField.text = user?.numberphone
+            cell.textField.tag = 3
+            cell.textField.keyboardType = .numberPad
             cell.textField.delegate = self
             return cell
             
         case .bioItem:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
             cell.titleLabel.text = "О себе"
+            cell.textField.text = user?.welcomeDescription
+            cell.textField.tag = 4
             cell.textField.delegate = self
             return cell
             
         case .passwordItem:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
             cell.titleLabel.text = "Пароль"
+            cell.textField.tag = 5
             cell.textField.delegate = self
             return cell
             
         case .saveButtonItem:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath) as! EditButtonCell
             cell.actionButton.setTitle("Сохранить", for: .normal)
+            cell.actionButton.addTarget(self, action: #selector(saveButtonAction), for: .touchUpInside)
             return cell
             
         case .none:
             return UITableViewCell()
         }
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("LOG: sda \(indexPath.row)")
-    }
-
 }
 
 extension EditProfileViewController: UITextFieldDelegate {
@@ -131,7 +159,57 @@ extension EditProfileViewController: UITextFieldDelegate {
 }
 
 extension EditProfileViewController {
-    func setProfileInfo() {
-        self.activityIndicator.stopAnimating()
+    @objc func saveButtonAction() {
+        
+        for i in 0..<tableView.numberOfRows(inSection: 0) {
+            if let cell = tableView.cellForRow(at: IndexPath(row: i, section: 0)) as? EditTextFieldCell {
+                let items = cell.textField.tag
+                switch items {
+                case 0:
+                    editUser.firstname = cell.textField.text
+                case 1:
+                    editUser.lastname = cell.textField.text
+                case 2:
+                    editUser.userEmail = cell.textField.text
+                case 3:
+                    editUser.numberphone = cell.textField.text
+                case 4:
+                    editUser.welcomeDescription = cell.textField.text
+                default:
+                    print("LOG: none")
+                }
+//                items.description.forEach() { a in print("LOG: \(cell.textField.text)") }
+            }
+        }
+        
+        presenter?.saveChangesProfile(user: editUser)
     }
+    
+    func setProfileInfo(user: User) {
+        DispatchQueue.main.async {
+            self.user = user
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+            self.activityIndicator.stopAnimating()
+        }
+    }
+    
+    func showEditProfileSuccess(user: User) {
+        DispatchQueue.main.async {
+            print("LOG: ОБНОВЛЕНИЕ СДЕЛАНО")
+        }
+    }
+    
+    func showEditProfileError(_ error: String) {
+        DispatchQueue.main.async {
+            print("ОШИБКА ПОЛУЧЕКНИЯ ПРОФИЛЯ ВЬЮ: \(error)")
+        }
+    }
+    
+    func showErrorProfileInfo(_ error: String) {
+        DispatchQueue.main.async {
+            print("ОШИБКА ПОЛУЧЕКНИЯ ПРОФИЛЯ ВЬЮ: \(error)")
+        }
+    }
+    
 }
