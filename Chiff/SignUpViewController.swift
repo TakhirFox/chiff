dd8596f533af8a2e04348fc4551777976eef0cc1
Zennex <60847855+TakhirFox@users.noreply.github.com>
2022-03-13 23:51:06 +0700

    Оформление таблицы

--- Chiff/SignUpViewController.swift
+++ Chiff/SignUpViewController.swift
@@ -14,26 +14,120 @@ protocol SignUpViewControllerProtocol: AnyObject {
 
 class SignUpViewController: BaseViewController, SignUpViewControllerProtocol {
     
+    private enum Items: Int {
+        case titleItem = 0
+        case emailItem = 1
+        case passwordItem = 2
+        case repeatPassItem = 3
+        case nextStepItem = 4
+    }
+    
+    var tableView: UITableView!
+    
     var presenter: SignUpPresenterProtocol?
     
     override func viewDidLoad() {
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        activityIndicator.startAnimating()
         
-        
-        
+        setupTableView()
         setupSubviews()
         setupConstraints()
     }
     
+    func setupTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .none
+        tableView.keyboardDismissMode = .onDrag
+        tableView.isHidden = true
+        tableView.register(TitleCell.self, forCellReuseIdentifier: "cell0")
+        tableView.register(EditAvatarCell.self, forCellReuseIdentifier: "cell1")
+        tableView.register(EditTextFieldCell.self, forCellReuseIdentifier: "cell2")
+        tableView.register(EditButtonCell.self, forCellReuseIdentifier: "cell3")
+        
+        tableView.isHidden = false
+        activityIndicator.stopAnimating()
+    }
+    
     func setupSubviews() {
-       
+        view.addSubview(tableView)
+        view.addSubview(activityIndicator)
     }
     
     func setupConstraints() {
-    
+        tableView.snp.makeConstraints { make in
+            make.top.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        activityIndicator.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
     }
+    
+}
+
+extension SignUpViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let items = Items(rawValue: indexPath.item)
+        switch items {
+        case .titleItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell0", for: indexPath) as! TitleCell
+            cell.titleLabel.text = "Создаем аккаунт"
+            return cell
+            
+        case .emailItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Email"
+//            cell.textField.text = user?.firstname
+            cell.textField.tag = 0
+            cell.textField.delegate = self
+            return cell
+            
+        case .passwordItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Пароль"
+//            cell.textField.text = user?.firstname
+            cell.textField.tag = 1
+            cell.textField.delegate = self
+            return cell
+            
+        case .repeatPassItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! EditTextFieldCell
+            cell.titleLabel.text = "Повторите пароль"
+//            cell.textField.text = user?.lastname
+            cell.textField.tag = 2
+            cell.textField.delegate = self
+            return cell
+            
+            
+        case .nextStepItem:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath) as! EditButtonCell
+            cell.actionButton.setTitle("Далее", for: .normal)
+            cell.actionButton.addTarget(self, action: #selector(nextStepButtonAction), for: .touchUpInside)
+            return cell
+            
+        case .none:
+            return UITableViewCell()
+        }
+    }
+    
+}
+
+extension SignUpViewController: UITextFieldDelegate {
+    
 }
 
 extension SignUpViewController {
-   
+    @objc func nextStepButtonAction() {
+        presenter?.routeToPersonalSignUpAction() // TODO: Не забыть передать заготовленные данные для последующей регистраии.
+    }
+    
 }
